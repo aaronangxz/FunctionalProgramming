6fe15a7a3b708691edb85bddb5baca37976be313
aaronangxz <angxze@gmail.com>
2023-02-28 01:46:32 +0800

    add some stuff

--- helloworld.hs
+++ helloworld.hs
@@ -1,6 +1,10 @@
-main :: IO ()
-main = putStrLn "Hello, World!"
+twice = (\ n -> n + n)
 
+main :: IO ()
+main = do
+    print "Hello, World!"
+    print . twice $ twice 7 {- Chain print with . to avoid parantheses -}
+    print $ twice $ twice 7 {- Chain print with $ , put values from right to left-}
 -- Execute with `stack helloworld.hs`
 -- OR
 -- ghc -o hello helloworld.hs
